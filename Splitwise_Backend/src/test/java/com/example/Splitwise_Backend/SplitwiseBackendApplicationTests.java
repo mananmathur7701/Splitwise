71dbc852730462cd4ce7fb55dd959f62632d36bb
mananmathur7701 <mmathur@argusoft.com>
2023-09-25 10:48:00 +0530

    Frontend and Backend Structures Added
    Created Backend With Spring Initializer and Frontend with Command Line In Angular.

---
+++ Splitwise_Backend/src/test/java/com/example/Splitwise_Backend/SplitwiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Splitwise_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
