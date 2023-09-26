99918dd4b7a0a7c62a67f0ec416f16b8302580f9
mananmathur7701 <mmathur@argusoft.com>
2023-09-26 16:45:57 +0530

    Lombok Added

--- Splitwise_Backend/src/main/java/com/example/Splitwise_Backend/entity/squareOffTransactions.java
+++ Splitwise_Backend/src/main/java/com/example/Splitwise_Backend/entity/squareOffTransactions.java
@@ -1,4 +1,15 @@
 package com.example.Splitwise_Backend.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class squareOffTransactions {
 }
