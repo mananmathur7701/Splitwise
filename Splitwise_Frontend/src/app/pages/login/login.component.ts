c1215499cdb22d3c81ac8df94f44dd550457c46c
AMRIT KUMAR <123072668+amr2t@users.noreply.github.com>
2023-10-25 18:16:08 +0530

    added header, footer, dashboard, etc.

---
+++ Splitwise_Frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,30 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+// export class LoginComponent {
+
+// }
+
+
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  user: any = {};
+
+  onSubmit() {
+    // Handle login logic here (e.g., check username and password)
+    console.log('User submitted:', this.user);
+  }
+}
+
