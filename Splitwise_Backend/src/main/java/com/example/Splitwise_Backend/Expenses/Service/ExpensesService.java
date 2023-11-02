405a605bd84d500f2c9079083988c078d74064fb
AMRIT KUMAR <123072668+amr2t@users.noreply.github.com>
2023-11-02 12:08:12 +0530

    Technical Changes In Some Api's

--- Splitwise_Backend/src/main/java/com/example/Splitwise_Backend/Expenses/Service/ExpensesService.java
+++ Splitwise_Backend/src/main/java/com/example/Splitwise_Backend/Expenses/Service/ExpensesService.java
@@ -5,11 +5,12 @@ import com.example.Splitwise_Backend.Expenses.Entity.Expenses;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 
 public interface ExpensesService {
 
-    public Expenses addExpense(Expenses expenses);
+    public Expenses addExpense(int groupId,float amountPaid,String comment);
     public String deleteExpense(int expenseId);
     public List<Expenses> showAllExpenses();
     public Expenses editExpense(int ExpenseId);
